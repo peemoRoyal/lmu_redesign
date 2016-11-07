965e149a12fcf7e9af148a7315f8c9a44f222a2b
pgleixner <pg@scherer-software.de>
2016-11-07 18:18:05 +0100

    sidemenu without JS-fancy

--- js/app.js
+++ js/app.js
@@ -30,7 +30,12 @@ function showDivs(n) {
 
 
 // past_visited sidemenu
+/* Set the width of the side navigation to 250px */
 function openNav() {
-    console.log(1);
-    alert(1)
+    document.getElementById("past-visited-menu").style.width = "85vw";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("past-visited-menu").style.width = "0";
 }
